74442d82ac7b40601a93edafc84871b535792e0a
alberto <alberto.femenias@udc.es>
2020-05-05 01:59:38 +0200

    mapita añadido al detalle de las alertas

--- app/src/main/java/es/udc/psi/drivesafeapp/ExpandedAlertActivity.java
+++ app/src/main/java/es/udc/psi/drivesafeapp/ExpandedAlertActivity.java
@@ -1,12 +1,22 @@
 package es.udc.psi.drivesafeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class ExpandedAlertActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class ExpandedAlertActivity extends AppCompatActivity implements OnMapReadyCallback  {
+
+    private GoogleMap mMap;
 
     ImageView mainImageView;
     TextView titleTV, descriptionTV;
@@ -19,12 +29,27 @@ public class ExpandedAlertActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.expanded_alert);
 
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync( this);
+
         mainImageView = findViewById(R.id.mainImageView);
         titleTV = findViewById(R.id.title);
         descriptionTV = findViewById(R.id.description);
 
         getData();
         loadData();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng coruña = new LatLng(43.3623, -8.4115);
+
+        mMap.addMarker(new MarkerOptions().position(coruña).title("Alerta"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(coruña, 12));
     }
 
     private void getData(){
